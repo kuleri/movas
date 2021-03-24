71485be388313a1cc76378bd3d3e7f84198060b5
SempaiEcchi <minecraftm665@gmail.com>
2021-03-24 20:57:47 +0100

    export

--- lib/movas.dart
+++ lib/movas.dart
@@ -1,10 +1,13 @@
 library movas;
 
 import 'package:movas/helper/logger.dart';
+import 'package:rxdart/rxdart.dart';
 
 export 'provider/provider.dart';
 export 'router/router.dart';
-
+export 'package:rxdart/rxdart.dart';
+export 'package:rxdart/streams.dart';
+export 'package:rxdart/subjects.dart';
 
 class Movas {
   static const int transitionDurationInMilliseconds = 500;
